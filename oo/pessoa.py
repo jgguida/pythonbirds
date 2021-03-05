82d9cf201cf7fdcc37ae0532be294361b7ab1db0
jgguida <jon.gui@aol.com>
2021-03-05 14:00:31 -0300

    Criação de métodos com objeto complexo

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,8 +1,17 @@
 class Pesoa:
+    def __init__(self, *filhos, nome=None, idade=None):
+        self.nome = nome
+        self.idade = idade
+        self.filhos = list(filhos)
+
+
     def cumprimentar(self):
         return 'Olá'
 
 
 if __name__ == '__main__':
-    p = Pesoa()
-    print(p.cumprimentar())
+    jonathan = Pesoa(nome='Jonathan')
+    sueli = Pesoa(jonathan, nome='Jonathan')
+    for filho in sueli.filhos:
+        print(filho.nome)
+
